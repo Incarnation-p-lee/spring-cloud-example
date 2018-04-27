dda814fef597ebbc192dde8f65b09cc1b9f12ba1
Pan Li <incarnation.p.lee@outlook.com>
2018-04-27 22:29:49 +0800

    Optimize the pom.xml
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- script/run-spring-cloud.sh
+++ script/run-spring-cloud.sh
@@ -3,12 +3,15 @@
 java -jar service-discovery/target/service-discovery-1.0-SNAPSHOT.jar --server.port=9999 &
 sleep 20
 
-java -jar ./web-application/target/web-application-1.0-SNAPSHOT.jar --server.port=8001 &
+java -jar web-application/target/web-application-1.0-SNAPSHOT.jar --server.port=8001 &
 sleep 20
 
-java -jar ./web-application/target/web-application-1.0-SNAPSHOT.jar --server.port=8002 &
+java -jar web-application/target/web-application-1.0-SNAPSHOT.jar --server.port=8002 &
 sleep 20
 
 java -jar web-application-backup/target/web-application-backup-1.0-SNAPSHOT.jar --server.port=8111 &
+sleep 20
+
+java -jar ribbon-consumer/target/ribbon-consumer-1.0-SNAPSHOT.jar --server.port=8003 &
 sleep 20
 

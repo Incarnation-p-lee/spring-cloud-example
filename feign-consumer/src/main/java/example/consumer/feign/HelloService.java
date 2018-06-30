1cd6869e33278323779bde82d3423bbd0b93d46c
Pan Li <incarnation.p.lee@outlook.com>
2018-06-30 11:57:45 +0800

    Add feign consumer with interface define.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

---
+++ feign-consumer/src/main/java/example/consumer/feign/HelloService.java
@@ -0,0 +1,11 @@
+package example.consumer.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("hello-service")
+public interface HelloService {
+
+    @RequestMapping("/hello")
+    String sayHello();
+}

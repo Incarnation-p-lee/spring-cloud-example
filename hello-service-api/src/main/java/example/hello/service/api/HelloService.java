40dabc01dbf06f16392e4f573937272d067a8918
Pan Li <incarnation.p.lee@outlook.com>
2018-07-03 23:09:33 +0800

    Add basic Zuul gateway module.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- hello-service-api/src/main/java/example/hello/service/api/HelloService.java
+++ hello-service-api/src/main/java/example/hello/service/api/HelloService.java
@@ -2,7 +2,6 @@ package example.hello.service.api;
 
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/refactor")
 public interface HelloService {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)

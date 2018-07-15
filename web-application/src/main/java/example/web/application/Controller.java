a649307be6325b2d5ea11cf840136bbc7243005f
Pan Li <incarnation.p.lee@outlook.com>
2018-07-15 22:53:39 +0800

    Apply config server for each module.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- web-application/src/main/java/example/web/application/Controller.java
+++ web-application/src/main/java/example/web/application/Controller.java
@@ -4,7 +4,10 @@ import example.hello.service.api.HelloService;
 import example.hello.service.api.User;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 

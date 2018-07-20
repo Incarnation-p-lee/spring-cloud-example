0d1dd8c4c75627e325bc0c5f857becb414d7e019
Pan Li <panli@microsoft.com>
2018-07-20 14:46:10 +0800

    Add refresh configuration to zuul-gateway.
    Signed-off-by: Pan Li <panli@microsoft.com>

--- zuul-gateway/src/main/java/example/zuul/gateway/AccessFilter.java
+++ zuul-gateway/src/main/java/example/zuul/gateway/AccessFilter.java
@@ -3,15 +3,23 @@ package example.zuul.gateway;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
+@RefreshScope
+@Component
 public class AccessFilter extends ZuulFilter {
 
     private static final String FILTER_BEFORE = "pre";
 
     private static final Integer FIRST_FILTER = 0;
+
+    @Value("${required.parameter}")
+    private String requiredParameter;
 
     @Override
     public String filterType() {
@@ -33,19 +41,19 @@ public class AccessFilter extends ZuulFilter {
         final RequestContext context = RequestContext.getCurrentContext();
         final HttpServletRequest request = context.getRequest();
 
-        log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+        final Object parameter = request.getParameter(this.requiredParameter);
 
-        final Object token = request.getParameter("token");
-
-        if (token == null) {
-            log.warn("detected null token");
+        if (parameter == null) {
+            log.warn("detected null parameter");
 
             context.setSendZuulResponse(false);
             context.setResponseStatusCode(401);
 
             return null;
         } else {
-            log.info("obtain token");
+            log.info("obtain parameter {} with value {}", this.requiredParameter, parameter);
+            log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+
         }
 
         return null;
--- zuul-gateway/src/main/java/example/zuul/gateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/example/zuul/gateway/ZuulGatewayApplication.java
@@ -3,7 +3,6 @@ package example.zuul.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -11,10 +10,5 @@ public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayApplication.class, args);
-    }
-
-    @Bean
-    public AccessFilter accessFilter() {
-        return new AccessFilter();
     }
 }
